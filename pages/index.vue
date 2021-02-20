2da56d061577e1c09167260efa59e54c479d8990
tevfiksirin <tevfiksirin@gmail.com>
2021-02-20 14:13:16 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <h1>Blog Posts</h1>
-    <ul>
-      <li v-for="article of articles" :key="article.slug">
+  <div class="container mx-auto space-y-10">
+    <h1 class="text-center text-3xl">Blog Posts</h1>
+    <ul class="lg:grid lg:grid-cols-2 lg:gap-4">
+      <li class="" v-for="article of articles" :key="article.slug">
         <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
-          <img :src="article.img" />
-          <div>
-            <h2>{{ article.title }}</h2>
-            <!-- <p>by {{ article.author.name }}</p> -->
-            <p>{{ article.description }}</p>
+          <div class="border-2 lg:flex">
+            <img
+              class="w-full h-48 object-cover object-top"
+              :src="article.img"
+            />
+            <div>
+              <h2>{{ article.title }}</h2>
+              <!-- <p>by {{ article.author.name }}</p> -->
+              <p>{{ article.description }}</p>
+            </div>
           </div>
         </NuxtLink>
       </li>
