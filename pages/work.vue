baf6bbb9302ab7139751ddbf2256539035c732b1
tevfiksirin <tevfiksirin@gmail.com>
2021-03-03 17:56:59 +0300

    Update work.vue

--- pages/work.vue
+++ pages/work.vue
@@ -3,16 +3,19 @@
     <div class="flex items-center justify-center h-64 bg-orange-600">
       <h1 class="pt-16 pb-16 text-5xl text-white">{{ $t('menu.work') }}</h1>
     </div>
-    <div class="container p-16 mx-auto">
-      <img v-img:group class="object-cover w-64 h-64" src="/img/first.jpg" />
-      <img v-img:group class="object-cover w-64 h-64" src="/img/second.jpg" />
-    </div>
-    <div class="relative" style="padding-top: 56.25%">
-      <iframe
-        class="absolute inset-0 w-full h-full p-16"
-        src="https://www.youtube-nocookie.com/embed/FMrtSHAAPhM"
-        frameborder="0"
-      ></iframe>
+    <div class="container flex justify-center gap-4 p-16 mx-auto">
+      <nuxt-img
+        sizes="sm:100vw md:50vw lg:400px"
+        v-img:group
+        class="object-cover w-64 h-64"
+        src="/img/first.jpg"
+      />
+      <nuxt-img
+        sizes="sm:100vw md:50vw lg:400px"
+        v-img:group
+        class="object-cover w-64 h-64"
+        src="/img/second.jpg"
+      />
     </div>
   </div>
 </template>
