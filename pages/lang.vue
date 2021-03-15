ad76c449222eee65cc4c586ca66c2123aa44a145
tevfiksirin <tevfiksirin@gmail.com>
2021-03-15 17:37:07 +0300

    deleted hover work

--- pages/lang.vue
+++ pages/lang.vue
@@ -3,7 +3,6 @@
     <div class="flex items-center justify-center h-64 bg-indigo-500 gap-14">
       <h1 class="text-3xl text-white">{{ $t('messages.welcome') }}</h1>
     </div>
-    <Hover />
   </div>
 </template>
 <script>
