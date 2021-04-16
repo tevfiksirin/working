419cf654c15fb0ce2b088b2accbf441d2ffe0a17
tevfiksirin <tevfiksirin@gmail.com>
2021-04-17 00:03:24 +0300

    a11y changed for button

--- components/MyButton.vue
+++ components/MyButton.vue
@@ -9,30 +9,30 @@ export default {
   props: {
     round: {
       type: Boolean,
-      default: false
+      default: false,
     },
     type: {
       type: String,
-      default: 'primary'
+      default: 'primary',
     },
     text: {
       type: String,
-      default: 'Button'
-    }
+      default: 'Button',
+    },
   },
   computed: {
-    btnClass () {
+    btnClass() {
       const classes = []
       if (this.round) {
         classes.push('rounded-full')
       }
       if (this.type === 'primary') {
-        classes.push('bg-blue-500 hover:bg-blue-700')
+        classes.push('bg-blue-500 text-gray-900 hover:bg-blue-700')
       } else if (this.type === 'secondary') {
         classes.push('bg-gray-500 hover:bg-gray-700')
       }
       return classes.join(' ')
-    }
-  }
+    },
+  },
 }
 </script>
