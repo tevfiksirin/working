bfe310fb08fb05d14094efe29bf717fd149686f1
tevfiksirin <tevfiksirin@gmail.com>
2021-07-07 14:04:14 +0300

    create button component

---
+++ components/atoms/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button class="p-2 mt-4 text-white bg-red-500">{{ text }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+}
+</script>
--- pages/lang.vue
+++ pages/lang.vue
@@ -3,6 +3,9 @@
     <div class="flex items-center justify-center h-64 bg-indigo-500 gap-14">
       <h1 class="text-3xl text-white">{{ $t('messages.welcome') }}</h1>
     </div>
+    <div>
+      <AtomsButton text="Selam" />
+    </div>
   </div>
 </template>
 
